e4eb09d417ced2327afc081cdf2f1184a8dfb385
rajevi05 <rajevi05@INL8RD9DC2.enterprisenet.org>
2017-01-12 18:24:41 +0530

    Adding filter to JSON table data

--- js/custom/table.js
+++ js/custom/table.js
@@ -3,27 +3,20 @@ app.controller("tableController", function($scope, $http) {
 	$http.get('data.json').success(
 			function(data) {
 				$scope.personalDetails = data;
-				$scope.categorySelects = [ '--Select--', 'Category A',
-						'Category B', 'Category C' ];
-				$scope.categorySelectval = '--Select--';
-				$scope.dateRangeSelects = [ '--Select--', 'Full Year 2017',
-						'Latest Month', 'Latest 52 Weeks', 'Latest 26 Weeks',
+				$scope.categorySelects = [ 'Category A', 'Category B',
+						'Category C' ];
+				$scope.category = '';
+				$scope.dateRangeSelects = [ 'Full Year 2017', 'Latest Month',
+						'Latest 52 Weeks', 'Latest 26 Weeks',
 						'Year Ago 52 Weeks', 'Year Ago 4 Weeks',
 						'Year Ago 26 Weeks' ];
-				$scope.dateRangeSelectval = '--Select--';
-				$scope.geographySelects = [ '--Select--', 'Africa', 'Asia',
-						'Australia', 'Europe', 'USA' ];
-				$scope.geographySelectval = '--Select--';
+				$scope.dateRangeSelectval = '';
+				$scope.geographySelects = [ 'Africa', 'Asia', 'Australia',
+						'Europe', 'USA' ];
+				$scope.geography = '';
 			});
 	$scope.results = [];
-	$scope.findValue = function() {
-		angular.forEach($scope.personalDetails.category, function(value, key) {
-			if (key === enteredValue) {
-				$scope.results.push({
-					serial : key,
-					owner : value[0].Owner
-				});
-			}
-		});
+	$scope.optimizedSearch = function(personalDetail) {
+		return personalDetail.User.Stats[0].active === "1";
 	};
 });
